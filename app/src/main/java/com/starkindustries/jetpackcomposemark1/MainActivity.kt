51b91b14db46442b766d30d7ce37b32b2522694c
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-08-05 01:13:33 +0530

    Initial commit

--- app/src/main/java/com/starkindustries/jetpackcomposemark1/MainActivity.kt
+++ app/src/main/java/com/starkindustries/jetpackcomposemark1/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.starkindustries.jetpackcomposemark1
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,13 +8,30 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.starkindustries.jetpackcomposemark1.ui.theme.JetPackComposeMark1Theme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
         }
     }
+}
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun FirstCompose(){
+    Text(text = "I am Ironman",
+        fontFamily = FontFamily.Monospace,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Right,
+        maxLines = 2
+    )
 }
